1e589284df0f18aa19906fa490688634b950aa57
newObjectccc <897333949@qq.com>
2024-01-12 18:52:27 +0800

    feat(preload): register clipboard feature to ctxBridge

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { clipboard, contextBridge } from 'electron';
+import path from 'path';
+
+contextBridge.exposeInMainWorld('electron', {
+  clipboard: {
+    readText: clipboard.readText,
+    writeText: clipboard.writeText
+  },
+  imgSrcMap: new Map<string, string>([
+    ['xf', path.join(__dirname, '../../src/', 'assets', 'xf.png')]
+  ])
+});
