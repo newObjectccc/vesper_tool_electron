666dc73185e9e3f5fa01bb82660572ac573df22b
newObjectccc <897333949@qq.com>
2024-01-12 18:55:35 +0800

    fix(mainView): fix ui render& provide clipboard ctx

--- src/renderer/main-view/mainView.tsx
+++ src/renderer/main-view/mainView.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useMounted } from '../../hooks/useMounted';
-
+import styles from './mainView.module.css';
 interface MainViewProps {
   title: string;
 }
@@ -10,12 +11,29 @@ const MainView = (props: MainViewProps) => {
   });
 
   const { title } = props;
+  const { clipboard, imgSrcMap } = window.electron;
+  const [clipboardContent, setClipboardContent] = useState(clipboard.readText());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentClipboardContent = clipboard.readText();
+      if (currentClipboardContent !== clipboardContent) {
+        setClipboardContent(currentClipboardContent);
+        console.log('Clipboard changed:', currentClipboardContent);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [clipboardContent]);
   return (
-    <>
-      <h1>💖 Hello World!</h1>
-      <p>Welcome to your Electron application.</p>
-      {title && <p>title: {title}</p>}
-    </>
+    <div>
+      {/* <h1>💖 Hello World!</h1> */}
+      {/* <p>Welcome to your Electron application.</p>
+      {title && <p>title: {title}</p>} */}
+      <img className={styles['img']} src={imgSrcMap.get('xf')} alt="loading" />
+    </div>
   );
 };
 
